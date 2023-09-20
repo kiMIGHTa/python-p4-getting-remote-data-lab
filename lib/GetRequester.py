5ea22e413a57d249e3ce9c7f7ca1325f908aa32b
Dennis Kimaita <dkimaita22@gmail.com>
2023-09-20 06:28:43 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,18 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        url=self.url
+        response=requests.get(url)
+        return response.content
 
     def load_json(self):
+        data_list=[]
+        data_hub = json.loads(self.get_response_body())
+        for data in data_hub:
+            data_dict = {"name": data["name"], "occupation": data["occupation"]}
+            data_list.append(data_dict)
+        return data_list
         pass
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+# print(get_requester.get_response_body())
